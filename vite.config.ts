0487097ed350ec1d091a43cb0a532e5212f91051
NikolausAlbert-cloud <186523965+NikolausAlbert-cloud@users.noreply.github.com>
2025-06-03 07:39:28 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      include: 'src/assets/**/*.svg',
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@':  path.resolve(__dirname, './src'),
+    },
+  },
+});
